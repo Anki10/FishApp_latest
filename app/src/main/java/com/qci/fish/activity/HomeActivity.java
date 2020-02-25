4d5170a661239cb397675c2b8f9f5621c6fc5d9f
Anki1o <ankitmiglani10@gmail.com>
2020-02-25 21:41:07 +0530

    home change

--- app/src/main/java/com/qci/fish/activity/HomeActivity.java
+++ app/src/main/java/com/qci/fish/activity/HomeActivity.java
@@ -33,6 +33,7 @@ public class HomeActivity extends AppCompatActivity {
     private String click_type;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
